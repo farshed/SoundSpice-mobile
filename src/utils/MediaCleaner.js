a6d3987e35574a275e8896a10ae6f9bb051d2930
Faisal Arshed <faisal.arshed.28@gmail.com>
2020-07-31 15:03:44 +0500

    Filter flac files and fix for pound sym

--- src/utils/MediaCleaner.js
+++ src/utils/MediaCleaner.js
@@ -42,23 +42,23 @@ export default function cleanupMedia(media) {
 		(val) => !foldersToSkip.includes(getFolder(val.path).toLowerCase())
 	);
 	for (let i = 0; i < mediaFiles.length; i++) {
-		if (mediaFiles[i].title) {
-			folderName = getFolder(mediaFiles[i].path);
-			newMedia.push({
-				id: mediaFiles[i].duration + i,
-				duration: mediaFiles[i].duration,
-				url: mediaFiles[i].path,
-				title:
-					mediaFiles[i].title ||
-					mediaFiles[i].fileName.replace(/.mp3|.aac|.wav|.amr|.flac/, ''),
-				artwork: mediaFiles[i].cover || null,
-				artist: mediaFiles[i].author === '<unknown>' ? 'unknown' : mediaFiles[i].author,
-				album: mediaFiles[i].album === '<unknown>' ? 'unknown' : mediaFiles[i].album,
-				index: i,
-				folder: folderName
-				// pitchAlgorithm: PITCH_ALGORITHM_MUSIC
-			});
-		}
+		// if (mediaFiles[i].title) {
+		if (mediaFiles[i].fileName.includes('.flac')) continue;
+		folderName = getFolder(mediaFiles[i].path);
+		newMedia.push({
+			id: mediaFiles[i].duration + i,
+			duration: mediaFiles[i].duration,
+			url: mediaFiles[i].path,
+			title: (
+				mediaFiles[i].title || mediaFiles[i].fileName.replace(/.mp3|.aac|.wav|.amr|.flac/, '')
+			).replace('#', ''),
+			artwork: mediaFiles[i].cover || null,
+			artist: mediaFiles[i].author === '<unknown>' ? 'unknown' : mediaFiles[i].author,
+			album: mediaFiles[i].album === '<unknown>' ? 'unknown' : mediaFiles[i].album,
+			index: i,
+			folder: folderName
+			// pitchAlgorithm: PITCH_ALGORITHM_MUSIC
+		});
 	}
 	return newMedia;
 }
